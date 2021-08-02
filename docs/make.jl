e16875890fec7235e7d182714d2e69f481380257
Wiktor Phillips <wsphillips@gmail.com>
2021-08-02 16:11:09 +0200

    add doc building

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,7 @@ using Documenter, Conductor
 
 makedocs(sitename="Conductor.jl",
          pages = ["Home" => "index.md"])
+
+deploydocs(
+    repo = "github.com/wsphillips/Conductor.jl.git",
+    devbranch = "main")
