45d56363bff41a57423e1ad3edfe53eda053d87c
Wiktor Phillips <wsphillips@gmail.com>
2022-06-12 22:51:24 -0400

    cleanup

--- demo/pinskyrinzel.jl
+++ demo/pinskyrinzel.jl
@@ -1,7 +1,7 @@
 
 include(joinpath(@__DIR__, "traub_kinetics.jl"))
 
-import Unitful: µF, pA, µA, nA
+import Unitful: µF, pA, µA, nA, µS
 
 @parameters ϕ = 0.13 β = 0.075 p = 0.5
 
@@ -23,7 +23,7 @@ pinsky_nav_kinetics = [convert(Gate{SteadyState}, nav_kinetics[1]), nav_kinetics
 pinsky_ca_kinetics = [ca_kinetics[1]]
 @named CaS = IonChannel(Calcium, pinsky_ca_kinetics)
 
-is_val = ustrip(Float64, µA, -0.5µA)/p
+is_val = ustrip(Float64, µA, 0.75µA)/p
 @named Iₛ = IonCurrent(NonIonic, is_val, dynamic = false)
 soma_holding = Iₛ ~ is_val
 
@@ -51,13 +51,10 @@ dendrite_holding = I_d ~ id_val
                              extensions = [calcium_conversion],
                              stimuli = [dendrite_holding])
 
-dumb_Eleak = EquilibriumPotential(Leak, 20mV)
 
-@named dummy = Compartment(Vₘ, [leak(1mS/cm^2)], [dumb_Eleak])
-
-@named gc_soma = AxialConductance([Gate(SteadyState, 1/p, name = :ps)],
+@named gc_soma = AxialConductance([Gate(SteadyState, inv(p), name = :ps)],
                                   max_g = gc_val)
-@named gc_dendrite = AxialConductance([Gate(SteadyState, 1/(1-p), name = :pd)],
+@named gc_dendrite = AxialConductance([Gate(SteadyState, inv(1-p), name = :pd)],
                                       max_g = gc_val)
 
 soma2dendrite = Junction(soma => dendrite, gc_soma, symmetric = false);
@@ -65,15 +62,16 @@ dendrite2soma = Junction(dendrite => soma, gc_dendrite, symmetric = false);
 
 @named mcneuron = MultiCompartment([soma2dendrite, dendrite2soma])
 
-simp = Simulation(mcneuron, time=2000ms, return_system = true)
 
-prob = ODAEProblem(simp, [], (0., 2000), [])
 # Uncomment to explicitly use the same u0 as published
 # prob = ODAEProblem(simp, [-4.6, 0.999, 0.001, 0.2, -4.5, 0.01, 0.009, .007], (0., 2000), [])
 
 using OrdinaryDiffEq, Plots
-# Pinsky & Rinzel originally solved using RK4 and dt=0.05
-#sol = solve(prob, RK4(), dt=0.05, maxiters=1e9)
+
+prob = Simulation(mcneuron, time=2000ms)
+
+# Note: Pinsky & Rinzel originally solved using RK4 and dt=0.05
+# sol = solve(prob, RK4(), dt=0.05, maxiters=1e9)
 sol = solve(prob, RadauIIA5(), abstol=1e-9, reltol=1e-9)
 plot(sol, vars=[soma.Vₘ])
 
@@ -85,20 +83,25 @@ import Conductor: NMDA, AMPA, HeavisideSum
 @named NMDAChan = SynapticChannel(NMDA,
                 [Gate(SteadyState, inv(1 + 0.28*exp(-0.062(Vₘ - 60.))); name = :e),
                  Gate(HeavisideSum, threshold = 10mV, saturation = 150; name = :S),
-                 Gate(SteadyState, 1/p, name = :pnmda)],
-                max_s = 0mS, aggregate = true)
+                 Gate(SteadyState, inv(1-p), name = :pnmda)],
+                 max_s = 0mS, aggregate = true)
 
 @named AMPAChan = SynapticChannel(AMPA,
                                 [Gate(HeavisideSum, threshold = 20mV, saturation = 2;
                                       name = :u),
-                                 Gate(SteadyState, 1/p, name = :pnmda)],
-                                max_s = 0mS, aggregate = true)
+                                 Gate(SteadyState, inv(1-p), name = :pampa)],
+                                 max_s = 0mS, aggregate = true)
+
+# To simulate constant NMDA activation, we make a fake suprathreshold cell
+dumb_Eleak = EquilibriumPotential(Leak, 20mV)
+@named dummy = Compartment(Vₘ, [leak(1mS/cm^2)], [dumb_Eleak])
 
 ESyn = EquilibriumPotential(NMDA, 60mV, name = :syn)
-topology = [Synapse(dummy => mcneuron.dendrite, NMDAChan(1.25mS), ESyn)]
+topology = [Synapse(dummy => mcneuron.dendrite, NMDAChan(1.0µS), ESyn)]
 
 network = NeuronalNetworkSystem(topology)
 
-simp = Simulation(network, time=2000ms, return_system = true)
-prob = ODAEProblem(simp, [], (0., 2000), [])
+prob = Simulation(network, time=5000ms)
+sol = solve(prob, RadauIIA5(), abstol=1e-8, reltol=1e-8)
+plot(sol, plotdensity=25000, vars=[mcneuron.soma.Vₘ, mcneuron.dendrite.Vₘ], size=(1200,800))
 
