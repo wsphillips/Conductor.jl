0875505b72916cb3b1645052e812d630fd5e7692
Wiktor Phillips <wsphillips@gmail.com>
2022-06-14 12:33:38 -0400

    update doc workflows (#33)

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,12 @@
 using Documenter, Conductor
 
 makedocs(sitename="Conductor.jl",
+         doctest = false,
+         modules = [Conductor],
          pages = ["Home" => "index.md"])
 
 deploydocs(
     repo = "github.com/wsphillips/Conductor.jl.git",
-    devbranch = "main")
+    devbranch = "main",
+    push_preview=true
+)
