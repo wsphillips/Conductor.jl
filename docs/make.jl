976f7cb86b3e4839451a65b9bff842ea6519c4ea
Wiktor Phillips <wsphillips@gmail.com>
2022-06-17 16:30:26 -0400

    .

--- docs/make.jl
+++ docs/make.jl
@@ -3,10 +3,12 @@ using Documenter, Conductor
 makedocs(sitename="Conductor.jl",
          doctest = true,
          modules = [Conductor],
+         strict = true,
          pages = ["Home" => "index.md",
                   "Basics" => "basics.md",
                   "Core" => ["Primitives" => "core/primitives.md",
                              "Gates"      => "core/gates.md",
+                             "Conductances" => "core/conductances.md",
                              "Compartments" => "core/compartments.md",
                              "Networks"     => "core/networks.md",]])
 
