51c94f9c225e8307758c8d5a89c7db00c0b06fb9
Wiktor Phillips <wsphillips@gmail.com>
2022-08-01 12:30:16 -0400

    .

--- demo/lifneuron.jl
+++ demo/lifneuron.jl
@@ -1,21 +1,14 @@
 
-using Conductor, ModelingToolkit
+using Conductor, ModelingToolkit, OrdinaryDiffEq, Plots
 import Unitful.ms
+
 dynamics = Conductor.LIF(-75.0, tau_membrane = 10.0, tau_synaptic = 10.0, threshold = -55.0, resistance = 0.1, stimulus = 500)
-
 @named neuron = CompartmentSystem(dynamics)
-
 neuron_pop = [CompartmentSystem(dynamics) for _ in 1:10]
 net = NetworkTopology(neuron_pop);
 
 sim = Simulation(neuron, time = 300ms, return_system = false)
-
-using OrdinaryDiffEq, Plots
-
-# LIF neuron -- (fixed time step only for now)
 sol = solve(sim, Rosenbrock23(), dt = 0.5);
-
-# Superimpose spikes on top of voltage
-plot(sol[neuron.V] + sol[neuron.S]*(70))
+plot(sol[neuron.V] + sol[neuron.S]*(70)) # Superimpose spikes on top of voltage
 
 
