2801e7e9e0c1899d752708826cab229768443426
Wiktor Phillips <wsphillips@gmail.com>
2022-11-03 09:08:55 -0400

    .

--- src/simulation.jl
+++ src/simulation.jl
@@ -37,6 +37,48 @@ function Simulation(neuron::CompartmentSystem{LIF}; time::Time, return_system = 
 end
 
 indexof(sym, syms) = findfirst(isequal(sym), syms)
+function indexmap(syms, ref)
+    idxs = similar(syms, Int64)
+    for i in eachindex(syms)
+        idxs[i] = indexof(syms[i], ref)
+    end
+    return idxs
+end
+
+spike_check(V,Vprev) = V >= 10 && Vprev < 10
+
+function spike_check_callback(integrator, Vm_idxs)
+    Vm = view(integrator.u, Vm_idxs)
+    Vprev = view(integrator.uprev, Vm_idxs)
+    return spike_check.(Vm, Vprev)
+end
+
+function spike_propagation_callback(integrator, S)
+    mg = multigraph(integrator.p) # multilayered graph of weights (one layer per synapse type)
+
+    # each synaptic conductance has a parameter value OR cicular buffer of spike input
+    # by default the value is zero (no spikes) so we may need a way to reset on the dt after a spike
+    view_of_input_ps = view(integrator.p, input_ps_idxs)
+
+    Vth = 10.0 # eventually pull stored parameters for spike threshold
+
+    for layer in keys(mg)
+        adjm = mg[layer]
+        
+        rows = rowvals(adjm) # presynaptic neuron indexes for each stored value
+        vals = nonzeros(adjm) # weights
+        n = size(adjm, 1) # n columns
+        
+        for j in 1:n
+            for i in nzrange(adjm, j)
+                w = vals[i]
+                spiked = S[rows[i]] 
+                weighted_sum = sum(w[spiked])
+                # synaptic_input_parameter_of_neuron_j = weighted_sum
+            end
+        end
+    end
+end
 
 function Simulation(network::NeuronalNetworkSystem; time::Time, return_system = false,
                     jac = false, sparse = false, parallel = nothing)
@@ -46,19 +88,21 @@ function Simulation(network::NeuronalNetworkSystem; time::Time, return_system = 
 
     dvs = states(simplified)
     ps = parameters(simplified)
+    
+    # extract the indexes of somatic voltage
     neuron_Vms = voltage.(neurons(get_topology(network)))
-    Vm_idxs = similar(neuron_Vms, Int64)
-     
-    for i in eachindex(neuron_Vms)
-        Vm_idxs[i] = indexof(neuron_Vms[i], dvs)
-    end
+    Vm_idxs = indexmap(neuron_Vms, dvs)
+    
+    # create an affect!(integrator) that returns a bit vector of spike events
+    sc_cb = Base.Fix2(spike_check_callback, Vm_idxs) # function of integrator
+
 
     # Vm_view = view(u, Vm_idxs) # add to head of callback functions
 
-    if return_system
-        return simplified
-    else
-        @info repr("text/plain", simplified)
-        return ODEProblem(simplified, [], (0.0, t_val), []; jac, sparse, parallel)
-    end
+    #if return_system
+    #    return simplified
+    #else
+    #    @info repr("text/plain", simplified)
+    #    return ODEProblem(simplified, [], (0.0, t_val), []; jac, sparse, parallel)
+    #end
 end
