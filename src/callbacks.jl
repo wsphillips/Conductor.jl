72e346e673a67ef2be146c288ab4d250b0c1e5f2
Wiktor Phillips <wsphillips@gmail.com>
2022-11-08 15:46:01 -0500

    .

---
+++ src/callbacks.jl
@@ -0,0 +1,79 @@
+
+function indexmap(syms, ref)
+    idxs = similar(syms, Int64)
+    for i in eachindex(syms)
+        idxs[i] = indexof(syms[i], ref)
+    end
+    return idxs
+end
+
+simple_spike_check(V,Vprev) = V >= 10. && Vprev < 10.
+
+struct SimpleSpikeDetection
+    voltage_indexes::Vector{Int}
+end
+
+function SimpleSpikeDetection(network, simplified)
+    dvs = states(simplified)
+    ps = parameters(simplified)
+    topo = get_topology(network)
+    neuron_Vms = voltage.(neurons(topo))
+    Vm_idxs = indexmap(neuron_Vms, dvs)
+    return SimpleSpikeDetection(Vm_idxs)
+end
+
+function (ssd::SimpleSpikeDetection)(integrator)::BitVector
+    idxs = ssd.voltage_indexes
+    V = view(integrator.u, idxs)
+    Vprev = view(integrator.uprev, idxs)
+    return simple_spike_check.(V, Vprev)
+end
+
+struct SimpleExponentialResponse
+    state_indexes::Vector{Int}
+    graph
+end
+
+function SimpleExponentialResponse(network, simplified)
+    # calculate state indexes 
+    return SimpleExponentialResponse(state_indexes, graph)
+end
+
+function simple_spike_propagation!(states, S, graph)
+    rows = rowvals(graph) # presynaptic neuron indexes for each stored value
+    vals = nonzeros(graph) # weights
+    n = size(graph, 1) # n columns
+    for i in 1:n
+        for j in nzrange(graph, i)
+            S[rows[j]] || continue
+            states[i] += vals[j]
+        end
+    end
+end
+
+function (ser::SimpleExponentialResponse)(integrator, S)
+    idxs = ser.state_indexes
+    g = ser.graph
+    states = view(integrator.u, idxs)
+    spike_propagation_callback!(states, S, g)
+    return 
+end
+
+struct NetworkCallbacks{D,R,V}
+    spike_detection::D
+    spike_responses::Vector{R}
+    voltage_reset::V
+end
+
+function NetworkCallbacks(spike_detection, spike_responses, voltage_reset = identity)
+    return NetworkCallbacks(spike_detection, spike_responses, voltage_reset)
+end
+
+function (ncs::NetworkCallbacks)(integrator)
+    S = ncs.spike_detection(integrator)
+    for sr! in spike_responses
+        sr!(integrator, S)
+    end
+    ncs.voltage_reset(integrator)
+end
+
--- src/simulation.jl
+++ src/simulation.jl
@@ -45,59 +45,11 @@ Base.getindex(x::NetworkParameters, i) = x.ps[i]
 topology(x::NetworkParameters) = getfield(x, :topology)
 indexof(sym, syms) = findfirst(isequal(sym), syms)
 
-function indexmap(syms, ref)
-    idxs = similar(syms, Int64)
-    for i in eachindex(syms)
-        idxs[i] = indexof(syms[i], ref)
-    end
-    return idxs
-end
-
-spike_check(V,Vprev) = V >= 10 && Vprev < 10
-
-function discrete_spike_detection_callback(integrator, Vm_idxs)::BitArray{1}
-    Vm = view(integrator.u, Vm_idxs)
-    Vprev = view(integrator.uprev, Vm_idxs)
-    return spike_check.(Vm, Vprev)
-end
-
-# S is layer independent; graph and Isyn depend on synapse model
-function spike_propagation_callback!(Isyn, S, graph)
-    rows = rowvals(graph) # presynaptic neuron indexes for each stored value
-    vals = nonzeros(graph) # weights
-    n = size(graph, 1) # n columns
-    for i in 1:n
-        for j in nzrange(graph, i)
-            S[rows[j]] || continue
-            Isyn[i] += vals[j]
-        end
-    end
-end
-
-function generate_network_callbacks()
-    dvs = states(simplified)
-    ps = parameters(simplified)
-
-    # extract the indexes of somatic voltage
-    topo = get_topology(network)
-    multigraph = graph(topo)
-    neuron_Vms = voltage.(neurons(topo))
-    Vm_idxs = indexmap(neuron_Vms, dvs)
-    detect_spikes = Base.Fix2(discrete_spike_detection_callback, Vm_idxs)
-
-end
-
 function Simulation(network::NeuronalNetworkSystem; time::Time, return_system = false,
                     jac = false, sparse = false, parallel = nothing)
     t_val, simplified = simplify_simulation(network, time)
-
     cb = generate_network_callbacks(network, simplified)
     
-
-
-
-    # Vm_view = view(u, Vm_idxs) # add to head of callback functions
-
     #if return_system
     #    return simplified
     #else
