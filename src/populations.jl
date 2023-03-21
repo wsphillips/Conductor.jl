f7c7771b8c08bc0e7537d187a7374d53e516bbda
Wiktor Phillips <wsphillips@gmail.com>
2023-03-21 16:08:47 -0400

    revise Population constructor and Base methods

--- src/populations.jl
+++ src/populations.jl
@@ -1,7 +1,7 @@
 """
 $(TYPEDEF)
 
-An iterable population (or small group) of neurons.
+A population (or small group) of neurons.
 
 A `Population` is a lazy representation of neurons templated from a single model prototype.
 Iterating over a `Population` returns up to _n_ uniquely-named copies of the prototype that
@@ -12,11 +12,8 @@ distributions.
 $(FIELDS)
 """
 struct Population{T <: AbstractCompartmentSystem}
-    "The base neuron model."
-    proto::T
-    "The number of neurons in the population."
-    n::Int
-    "Name of the population."
+    neurons::Vector{T}
+    "Base name of the population."
     popname::Symbol
     "Population defaults"
     defs::Vector{Dict{Num, Any}}
@@ -24,34 +21,33 @@ struct Population{T <: AbstractCompartmentSystem}
     stimuli::Vector{Vector{Equation}}
 end
 
-function Population(neuron, n; name = Symbol(""))
-    Population(neuron, n, name, [Dict{Num, Any}() for _ in 1:n], [Equation[] for _ in 1:n])
+# Until MTK supports equivalent systems (loops), we scalarize the population on construction.
+function Population(neuron, n; name)
+    neurons = map(i -> rename(neuron, Symbol(name, :_, i)), 1:n) #same convention as @named
+    Population(neurons, name, [Dict{Num, Any}() for _ in 1:n], [Equation[] for _ in 1:n])
 end
 
 Base.nameof(p::Population) = getfield(p, :popname)
 Base.eltype(::Population{T}) where {T} = T
-Base.length(p::Population) = getfield(p, :n)
-prototype(p::Population) = getfield(p, :proto)
+Base.length(p::Population) = length(getfield(p, :neurons))
 
-function Base.iterate(p::Population{T}, state = 1) where {T}
+function Base.iterate(p::Population, state = 1)
     state > length(p) && return nothing
-    new_name = Symbol(nameof(p), '₊', nameof(prototype(p)), '_', state)
-    neuron = T(prototype(p); name = new_name, defaults = p.defs[state])
+    neuron = p.neurons[state]
     return (neuron, state + 1)
 end
 
-function Base.iterate(rP::Iterators.Reverse{Population{T}},
-                      state = length(rP.itr)) where {T <: AbstractCompartmentSystem}
+function Base.iterate(rP::Iterators.Reverse{Population},
+                      state = length(rP.itr))
     state < 1 && return nothing
     pop = rP.itr
-    new_name = Symbol(nameof(pop), '₊', nameof(prototype(pop)), '_', state)
-    neuron = SciMLBase.remake(prototype(p); name = new_name, defaults = p.defs[state])
+    neuron = pop.neurons[state]
     return (neuron, state - 1)
 end
 
 function Base.getindex(p::Population, i::Int)
     1 <= i <= length(p) || throw(BoundsError(p, i))
-    return MTK.rename(prototype(p), Symbol(nameof(p), '₊', nameof(prototype(p)), '_', i))
+    return p.neurons[i]
 end
 
 Base.firstindex(p::Population) = 1
@@ -59,6 +55,9 @@ Base.lastindex(p::Population) = length(p)
 Base.getindex(p::Population, i::Number) = p[convert(Int, i)]
 Base.getindex(p::Population, I) = [p[i] for i in I]
 
+############################################################################################
+
+# Setting specific distribution values with pair syntax
 function (pop::Population)(p::Pair{Num, D}) where {D <: Distribution}
     new_defs = [Dict{Num, Any}() for _ in 1:length(pop)]
     for neuron_defs in new_defs
@@ -67,6 +66,7 @@ function (pop::Population)(p::Pair{Num, D}) where {D <: Distribution}
     return @set pop.defs = merge.(pop.defs, new_defs)
 end
 
+# Setting specific default values with pair syntax
 function (pop::Population)(p::Pair{Num, Q}) where {Q <: Quantity}
     var, quant = p.first, p.second
     cunits = hasmetadata(var, ConductorUnits) ? getmetadata(var, ConductorUnits) : nothing
@@ -79,11 +79,13 @@ function (pop::Population)(p::Pair{Num, Q}) where {Q <: Quantity}
     return @set pop.defs = merge.(pop.defs, new_defs)
 end
 
+# set _distribution_ of a symbol in the population (?)
 function Base.setindex!(pop::Population, p::Pair{Num, D}, i::Int) where {D <: Distribution}
     var, dist = p.first, p.second
     push!(pop.defs[i], var => rand(dist))
 end
 
+# set explicit default of a symbol in the population
 function Base.setindex!(pop::Population, p::Pair{Num, Q}, i::Int) where {Q <: Quantity}
     var, quant = p.first, p.second
     cunits = hasmetadata(var, ConductorUnits) ? getmetadata(var, ConductorUnits) : nothing
